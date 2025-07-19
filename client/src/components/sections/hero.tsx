729e03810ba7e4aa965ec4728262121d417e9603
Swati Gupta <89257671+SwatiBGupta1110@users.noreply.github.com>
2025-07-19 14:26:06 +0100

    Update hero.tsx

--- client/src/components/sections/hero.tsx
+++ client/src/components/sections/hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
             <img 
             src="https://raw.githubusercontent.com/SwatiBGupta1110/swatis-data-science-portfolio/main/client/public/images/Headshot.jpeg"
             alt="Professional headshot of Swati Gupta"
-            className="w-8 h-8 rounded-full mx-auto mb-6 border-4 border-white/20 shadow-xl object-cover"
+            className="w-32 h-32 rounded-full mx-auto mb-6 border-4 border-white/20 shadow-xl object-cover"
             />
           </div>
           
